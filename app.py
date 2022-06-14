00fadd7a24af9bc42b6d1dc6b45e6fd5780dec6e
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-06-14 19:13:43 +0530

    Fixed the error "sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) no such table: user"

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
